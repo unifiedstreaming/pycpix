ac132d2f852f0cc294bee51df48b6aafc62aa1a2
Mark Ogle <mark@unified-streaming.com>
2018-07-04 16:47:20 +0200

    add url to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name = 'cpix',
-    version = '0.0.0',
-    description = 'CPIX',
-    long_description = readme,
-    author = 'Mark Ogle',
-    author_email = 'mark@unified-streaming.com',
-    packages = find_packages(exclude=('tests', 'docs'))
+    name="cpix",
+    version="0.0.0",
+    description="CPIX",
+    long_description=readme,
+    author="Mark Ogle",
+    author_email="mark@unified-streaming.com",
+    packages=find_packages(exclude=("tests", "docs")),
+    url="https://github.com/unifiedstreaming/pycpix"
 )
